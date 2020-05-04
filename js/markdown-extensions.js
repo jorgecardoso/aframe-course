1597b682e36c62581128c4dccd96ef412c01e1b2
Glitch (aframe-usj) <none>
2020-05-04 11:47:24 +0000

    Changed screenshot server address and defined it env variable.

--- js/markdown-extensions.js
+++ js/markdown-extensions.js
@@ -6,7 +6,7 @@ module.exports = {
             return text.replace(/:::exercise (.+?)\s(.+)?/g,
                 '<div class="example" > ' +
                 '<iframe style="position:absolute; top: 0; left: 0" data-src="$1" width="100%" height="100%"></iframe>' +
-                '<img src=\'https://fixed-chronometer.glitch.me/shot/?url=$1\'>'+
+                '<img src=\'' + process.env.PUPPETEER_ADDRESS +'/shot/?url=$1\'>'+
                     '<span style="background-color:white">Click to load.</span></div>' +
                 '</div>');
         }
@@ -19,7 +19,7 @@ module.exports = {
                 '*Example: $1* (<a href="examples/$1.html" target="_blank">open in new tab</a> | <a href="/viewsource/examples/$1.html" target="_blank">view source</a>):' +
                 '<div class="example"> ' +
                 '  <iframe data-src="/examples/$1.html" width="100%" height="100%"></iframe>' +
-                '  <div><img src=\'https://fixed-chronometer.glitch.me/shot/?url=https://aframe-usj.glitch.me/examples/$1.html\'>' +
+                '  <div><img src=\'' + process.env.PUPPETEER_ADDRESS + '/shot/?url=https://aframe-usj.glitch.me/examples/$1.html\'>' +
                 '  <span><strong>Click to load.</strong><br> Use keys \'w\', \'a\', \'s\', \'d\' to move around.<br>Mouse to look around.</span>' +
                 '</div></div>');
         }
